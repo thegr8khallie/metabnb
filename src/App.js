7cdde15b4f04856aa4f112c93a8578bba15ae7ce
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-11 21:26:01 +0100

    Feat: Nav component initialized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { Nav } from "./components";
 import "./scss/style.scss";
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,5 @@
+export const Nav = () => {
+    return (
+        <nav className="nav">Nav</nav>
+    )
+}
