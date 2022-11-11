fb785fff3a611d912d95b7c4659010c9334b0fdf
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-11 21:54:05 +0100

    Feat: Navbar markup done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,19 @@
+import metabnbLogo from '../assets/icons & vectors/metabnbNav.png'
 export const Nav = () => {
     return (
-        <nav className="nav">Nav</nav>
+        <nav className="nav">
+            <div className="metabnb-logo">
+                <img src={metabnbLogo} alt="Metabnb Logo" />
+            </div>
+            <ul className="nav-elements">
+                <li className="nav-element">Home</li>
+                <li className="nav-element">Place to stay</li>
+                <li className="nav-element">NFTs</li>
+                <li className="nav-element">Community</li>
+                <li className="nav-element btn-wrapper">
+                    <button className="connect-wallet-btn">Connect wallet</button>
+                </li>
+            </ul>
+        </nav>
     )
 }
