9c0b2106114d2cbc06b59eb7c70dcd8216d25ef3
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-12 06:05:38 +0100

    Refactor: More refactoring done to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,14 +6,16 @@ export const Nav = () => {
                 <div className="metabnb-logo">
                     <img src={metabnbLogo} alt="Metabnb Logo" />
                 </div>
-                <ul className="nav-elements">
-                    <li className="nav-element">Home</li>
-                    <li className="nav-element">Place to stay</li>
-                    <li className="nav-element">NFTs</li>
-                    <li className="nav-element">Community</li>
-                </ul>
-                {/* <div className="nav-element btn-wrapper"></div> */}
-                <button className="connect-wallet-btn">Connect wallet</button>
+                <div className="navbar-menu">
+                    <ul className="nav-elements">
+                        <li className="nav-element">Home</li>
+                        <li className="nav-element">Place to stay</li>
+                        <li className="nav-element">NFTs</li>
+                        <li className="nav-element">Community</li>
+                    </ul>
+                    {/* <div className="nav-element btn-wrapper"></div> */}
+                    <button className="connect-wallet-btn">Connect wallet</button>
+                </div>
             </div>
         </nav>
     )
