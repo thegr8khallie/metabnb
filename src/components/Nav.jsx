03eebb6bd43d640fc95c2e62404e4b2afc0c387b
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-12 05:46:14 +0100

    Refactor: Refactored Navbar. This is to help accomodate further styling.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,19 @@ import metabnbLogo from '../assets/icons & vectors/metabnbNav.png'
 export const Nav = () => {
     return (
         <nav className="nav">
-            <div className="metabnb-logo">
-                <img src={metabnbLogo} alt="Metabnb Logo" />
+            <div className="navbar">
+                <div className="metabnb-logo">
+                    <img src={metabnbLogo} alt="Metabnb Logo" />
+                </div>
+                <ul className="nav-elements">
+                    <li className="nav-element">Home</li>
+                    <li className="nav-element">Place to stay</li>
+                    <li className="nav-element">NFTs</li>
+                    <li className="nav-element">Community</li>
+                </ul>
+                {/* <div className="nav-element btn-wrapper"></div> */}
+                <button className="connect-wallet-btn">Connect wallet</button>
             </div>
-            <ul className="nav-elements">
-                <li className="nav-element">Home</li>
-                <li className="nav-element">Place to stay</li>
-                <li className="nav-element">NFTs</li>
-                <li className="nav-element">Community</li>
-            </ul>
-            {/* <div className="nav-element btn-wrapper"></div> */}
-            <button className="connect-wallet-btn">Connect wallet</button>
-
         </nav>
     )
 }
