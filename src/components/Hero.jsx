ff3a9410062c90feb4395898e00892b66b045f62
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-13 05:24:29 +0100

    Style: Desktop styling for Hero section done (Save for typography)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,15 @@ export const Hero = () => {
     const bannerImages = [mbtoken, metamask, opensea];
     return (
         <section className="hero-section">
-            <div className="herotext">
-                <h1>Rent a <span className="purple">Place</span> away from <span className="purple">home</span> in the <span className="purple">metaverse</span></h1>
-                <p>we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
-                <form className="search-location">
-                    <input type="text" placeholder="Search for location" />
-                    <button type="submit">Search</button>
-                </form>
+            <div className="main-hero-section">
+                <div className="hero-text">
+                    <h1>Rent a <span className="purple">Place</span> away from <span className="purple">home</span> in the <span className="purple">metaverse</span></h1>
+                    <p>we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
+                    <form className="search-location">
+                        <input type="text" placeholder="Search for location" />
+                        <button type="submit">Search</button>
+                    </form>
+                </div>
                 <ul className="hero-images">
                     {
                         heroImages.map((i, j) => {
